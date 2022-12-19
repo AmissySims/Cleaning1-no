99865034f19e0f678722e8952b5448ce2569ca9d
cbvfgtn217@gmail.com <cbvfgtn217@gmail.com>
2022-12-19 21:09:24 +0300

    cjhnbhjdrf

--- Cleaning1/Pages/ServicesListPage.xaml.cs
+++ Cleaning1/Pages/ServicesListPage.xaml.cs
@@ -47,6 +47,7 @@ namespace Cleaning1.Pages
             {
                 AddServBtn.Visibility = Visibility.Collapsed;
             }
+            GeneralCount.Text = DBConnect.db.Services.Local.Count().ToString();
         }
 
         private void CreateServBtn_Click(object sender, RoutedEventArgs e)
@@ -61,9 +62,113 @@ namespace Cleaning1.Pages
             NavigationService.Navigate(new OrderPage(ServiceList.SelectedItems.Cast<Services>()));
         }
 
+        public void Sort()
+        {
+            ObservableCollection<Services> serviceList = ServicesList;
+
+            if (CountCb == null)
+                return;
+            if(SortCb== null) return;
+            if(FilterCb== null) return;
+            if(FindTb == null) return;
+            if(SortCb.SelectedItem != null)
+            {
+                switch((SortCb.SelectedItem as ComboBoxItem).Tag) 
+                {
+                    case "1":
+                        serviceList = DBConnect.db.Services.Local;
+                        break;
+                        case "2":
+                        serviceList = new ObservableCollection<Services>(ServicesList.OrderBy(x => x.Deadline));
+                        break;
+                    default:
+                        break;
+                }
+            }
+            ServicesList = serviceList;
+            if (FilterCb.SelectedItem != null) 
+            {
+                switch ((FilterCb.SelectedItem as ComboBoxItem).Tag)
+                {
+                    case "1":
+                        serviceList = DBConnect.db.Services.Local;
+                        break;
+                    case "2":
+                        serviceList = new ObservableCollection<Services>(ServicesList.Where(x => x.Cost > 500));
+                        break;
+                    case "3":
+                        serviceList = new ObservableCollection<Services>(ServicesList.Where(x => x.Cost > 1000));
+                        break;
+                    case "4":
+                        serviceList = new ObservableCollection<Services>(ServicesList.Where(x => x.Cost > 2000));
+                        break;
+                    default:
+                        break;
+                }
+            }
+            ServicesList = serviceList;
+            if (FindTb.Text.Length > 0 && FindTb != null) 
+            {
+                serviceList = new ObservableCollection<Services>(ServicesList.Where(x => x.Title.ToLower().StartsWith(FindTb.Text.ToLower().ToLower())));
+            }
+            ServicesList = serviceList;
+
+            if(CountCb.SelectedIndex > -1 && serviceList.Count() > 0)
+            {
+                int selCount;
+                if ((CountCb.SelectedItem as ComboBoxItem).Content.ToString() == "Все")
+                    serviceList = DBConnect.db.Services.Local;
+               else
+                {
+                    selCount = Convert.ToInt32((CountCb.SelectedItem as ComboBoxItem).Content);
+                    serviceList = new ObservableCollection<Services>(serviceList.Skip(selCount * actPage).Take(selCount));
+                    if(serviceList.Count() == 0)
+                    {
+                        actPage--;
+                        Sort();
+                    }
+                }
+                
+
+            }
+            ServicesList = serviceList;
+
+            FoundCount.Text = serviceList.Count().ToString() + " из ";
+
+        }
         private void CountCb_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            actPage = 0;
+            Sort();
+        }
 
+        private void LeftBtn_Click(object sender, RoutedEventArgs e)
+        {
+            actPage--;
+            if(actPage <  0)
+                actPage = 0; 
+            Sort();
+        }
+
+        private void RightBtn_Click(object sender, RoutedEventArgs e)
+        {
+            actPage++;
+            Sort();
+        }
+
+        private void SortCb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Sort();
+        }
+
+        private void FilterCb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Sort();
+        }
+
+        private void FindTb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Sort();
         }
     }
 }
